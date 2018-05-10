4f3e8e116f9c06df941f9a8f81a59716ad7ca468
Craig Peterson <craig.b.peterson@gmail.com>
2018-05-10 14:52:48 -0700

    fixed tags so page can be styled

--- includes/foodtruck_postback.php
+++ includes/foodtruck_postback.php
@@ -5,43 +5,53 @@ include 'menu_objects.php';
 
 if(isset($_POST['submit'])){//show transaction result
     
-    /* 
+     
     echo '<pre>';
     var_dump($_POST);
     echo '</pre>';
-    */
+    
     
 }else{//show form
     global $config;
     
     //start the form:
-    echo '<form action="" method="post">';
+    echo '
+        <form action="" method="post">
+    ';
     
     foreach ($config->items as $item) 
     {//generate form block for each menu item
         echo '
-        <form action="" method="post">
-            <p class="menuitem">
+            <div class="menuitem">
                 <label>
-                <h3>' . $item->Name . '</h3>
-                Quantity:<br />
-                <input type="text" name="item1" required="required" />
+                <p class="itemname">' . $item->Name . '</p>
+                <p class="description">' . $item->Description . '</p>
+                <p>Quantity:<br />
+                    <input class="quantity" type="text" name="item_' . $item->ID . '" value="0" required="required" />
+                </p>
+                <div class="extras">Add Extras (25&cent; each):
                 <br />';
         foreach ($item->Extras as $extra) 
-        {//generate checkboxes for add-ons
-            echo '<input type="checkbox" name="' . $extra . '" value="' . $extra . '" /> '. $extra . '<br />';
+        {//generate checkboxes for extras
+            echo '
+                <input type="checkbox" name="item_' . $item->ID . '_extras[]" value="' . $extra . '" /> ' . $extra . '
+                <br />
+            ';
         }
+        echo '
+                </div>
+                </label>
+            </div>
+        ';
     }//end form block for each menu item
 
     //finish off the form:
     echo '
-    </label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Purchase" />
-            <input type="reset" />
-        </p>    
-    </form>                    
+            <div class="formbuttons">
+                <input type="submit" name="submit" value="Purchase" />
+                <input type="reset" />
+            </div>    
+        </form>
     ';
 
     
