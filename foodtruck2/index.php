5e5ecc197f70fb0e0228f8304d314a078c6619ef
robertohogue <36170635+robertohogue@users.noreply.github.com>
2018-05-14 22:26:15 -0700

    All coding added to layout
    I'm uploading this into a new folder mostly because I don't want to kill anything good. If you like it, move it into the root. Also, I broke the centering at some point but I'm way too road-weary and exhausted to figure it out. I know it's an easy fix and I don't want to go crazy...
    
    I have this demo up live at: http://lolfu.org/school/ITC250/food/foodtruck/index.php

---
+++ foodtruck2/index.php
@@ -0,0 +1,80 @@
+<?php 
+//index.php
+
+$foodTruckName = 'Food Truck Name';
+?>
+
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<title><?= $foodTruckName ?></title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!--[if IE 7]>
+		<link rel="stylesheet" type="text/css" href="css/ie7.css">	
+	<![endif]-->
+	<!--[if IE 6]>
+		<link rel="stylesheet" type="text/css" href="css/ie6.css">	
+	<![endif]-->
+</head>
+<body>
+	<div id="header"> <!-- start of header -->
+		<span class="signboard"></span>
+		<ul id="infos">
+			<li class="home"> 
+				<a href="index.html">HOME</a> 
+			</li>
+
+	       <li class="address">
+				<a href="contact.html">buford@foodtruck.com</a> 
+			</li>
+		</ul>
+		<a href="#" id="logo"></a>
+		<ul id="navigation">
+			<li><a href="#"><span>Home</span></a></li>
+			<li class="main current"><a href="index.php"><span>Menu</span></a></li>
+			<li><a href="#"><span>Booking</span></a></li>
+			<li><a href="#"><span>Blog</span></a></li>
+		</ul> <!-- /#navigation -->
+	</div> <!-- end of header -->
+	<div id="contents"> <!-- start of contents --> 
+		<h2 class="main-course"><span>Our Delicious Menu Items</span></h2>
+  
+        
+        
+    <div class="orderform">
+        <?php include "includes/foodtruck_postback.php" ?>
+    </div>
+    
+    </div>		
+	<div id="footer"> <!-- start of footer -->
+		<ul class="advertise">
+			<li class="delivery">
+				<h2>Hungry? We Deliver</h2>
+				<a href="#">Download our Menu</a>
+			</li>
+			<li class="event">
+				<h2>Party! Party!</h2>
+				<p>Celebrate your</p>
+				<p>Special Events with Us</p>
+			</li>
+			<li class="connect">
+				<h2>Let's Keep in Touch</h2>
+				<a href="http://facebook.com/freewebsitetemplates" target="_blank" class="fb" title="Facebook"></a> 
+				<a href="http://twitter.com/fwtemplates" target="_blank" class="twitr" title="Twitter"></a>
+			</li>
+		</ul>
+		<div>
+			<ul class="navigation">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Book an event</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">About</a></li>
+				<li class="last"><a href="#">Contact</a></li>
+			</ul>
+			<span>&copy; Copyright 2018. All Rights Reserved.</span>
+		</div>
+	</div> <!-- end of footer -->
+</body>
+</html>
